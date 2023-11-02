6da5d67ef384523c4fa001479e2663941d893adf
malek <alteermalk@gmail.com>
2023-11-02 13:13:17 +0200

    malek alteer / jehad eboo

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -9,13 +9,33 @@ namespace Binary_Search_Homework // don't edit this line!!!
     public class BinarySearchHomework // don't edit this line!!!
     {
         public static int TernarySearch(int[] arr, int key, int start, int end) // don't edit this line!!!
-                                                                                // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
-                                                                                // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
-                                                                                // من الممكن تمرير قيمة 0 لتجاهلهم
+                                                                               // يمكنك تجاهل برمترات البداية والنهاية إذا لا تريد استخدام الطريقة العودية 
+                                                                               // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
+                                                                               // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: write code for the ternary search algorithm and return the index of the element
-            return 2;
+            int Left = 0;
+            int Right = arr.Length - 1;
+
+            while (Left <= Right)
+            {
+                int mid1 = Left + (Right - Left) / 3;
+                int mid2 = Right - (Right - Left) / 3;
+
+                if (arr[mid1] == key) return mid1;
+
+                if (arr[mid2] == key) return mid2;
+
+                if (key < arr[mid1]) Right = mid1 - 1;
+
+                else if (key > arr[mid2]) Left = mid2 + 1;
+
+                else { Left = mid1 + 1; Right = mid2 - 1; }
+            }
+            return -1;
         }
+
+       
 
         public static int BinarySearchForCalculatingRepeated
             (int[] arr, int key, bool is_first, int start, int end) // don't edit this line!!!
@@ -23,7 +43,27 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                     // لكن لاتقوم بحذفهم أو التعديل عليهم هنا تحت اي ظرف!!!
                                                                     // من الممكن تمرير قيمة 0 لتجاهلهم
         {
-            //TODO: this methods is for getting the first accurence of the key and the last accurance
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] == key)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < key)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
             return -1;
         }
 
@@ -31,7 +71,26 @@ namespace Binary_Search_Homework // don't edit this line!!!
         {
             //TODO: write code to calculate the repeat count of a spacific element
             // make sure to use the previous method in this method
-            return -1;
+            int count = 0;
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] == key)
+                {
+                    count++;
+                    int i = mid - 1;
+                    while (i >= left && arr[i] == key) { count++; i--; }
+                    int j = mid + 1;
+                    while (j <= right && arr[j] == key) { count++; j++; }
+                    return count;
+                }
+                else if (arr[mid] < key) left = mid + 1;
+                else right = mid - 1;
+            }
+            return count;
         }
     }
 }
